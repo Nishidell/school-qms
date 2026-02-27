daba0138645da6a7bd961fd32d928a81750b39a2
Nishidell <166776404+Nishidell@users.noreply.github.com>
2026-02-27 13:33:41 +0800

    fix: remove backend root text to allow React to load

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,18 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/settings', require('./routes/settingRoutes'));
 
-app.get('/', (req, res) => {
-  res.send('School Queueing System API is running on XAMPP MySQL...');
+// --- DEPLOYMENT MERGE ---
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Catch-all route: Send everything else to React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
+
+/*app.get('/', (req, res) => {
+  res.send('School Queueing System API is running on XAMPP MySQL...');
+});*/
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
